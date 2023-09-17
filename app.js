85dc92d2277b7aa1c30e9f54301bdcf2f2893d8c
ghostsheep <psupacheep@gmail.com>
2023-09-17 13:03:45 +0700

    add claer btn, change id

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const buttons = document.querySelectorAll("#button");
+const displayResult = document.querySelector("#result")
+const clearBtn = document.querySelector('#clear')
+const equal = document.querySelector('#equal')
+let buttonValue = 0
+let result = 0
+displayResult.textContent = buttonValue;
 buttons.forEach(button => {
     button.addEventListener('click',()=>{
-        const buttonValue = button.value; 
+        buttonValue = button.value; 
         console.log("Button Value:", buttonValue);
+        // displayResult.textContent = buttonValue;
+    })
 })
+
+
+
+
+
+clearBtn.addEventListener('click',()=>{
+    buttonValue = 0
+    displayResult.textContent = buttonValue;
 })
